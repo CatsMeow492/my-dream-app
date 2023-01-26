9d228dcfeea8489cbfe3150c0f3763d1bd4d0e2b
Taylor Mohney <taylormohneytech@gmail.com>
2023-01-26 14:59:52 -0800

    218

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,1 +1,9 @@
-export class
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+
+}
