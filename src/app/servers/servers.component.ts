526da7eaba12f317e0f98437e31b902bc074f2bb
CatsMeow492 <calisurf243@hotmail.com>
2023-02-11 16:02:38 -0700

    2a2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,13 +10,20 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  username = "";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+    if (this.username.length > 0) {
+      this.username = "";
+    }
   }
   ngOnInit() {
 
+  }
+  onSetUsername() {
+    this.username = (<HTMLInputElement>event.target).value;
   }
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
