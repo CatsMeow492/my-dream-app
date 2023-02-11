eb275220811d20d73e65d6c88b47b2313425d109
CatsMeow492 <calisurf243@hotmail.com>
2023-02-11 14:16:22 -0700

    229

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -15,5 +16,8 @@ export class ServersComponent implements OnInit {
   }
   ngOnInit() {
 
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
